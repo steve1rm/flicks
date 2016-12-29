6232f5be5ff57aa0e590a56ccbaef3cce7c77e6c
Steve Mason <steve@androidbox.me>
2016-12-28 23:29:20 -0500

    added model classes for latest movie

---
+++ app/src/main/java/me/androidbox/flicks/model/ProductionCompanies.java
@@ -0,0 +1,26 @@
+package me.androidbox.flicks.model;
+
+/**
+ * Created by steve on 12/28/16.
+ */
+
+public class ProductionCompanies {
+    private int id;
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/me/androidbox/flicks/model/ProductionCountries.java
@@ -0,0 +1,26 @@
+package me.androidbox.flicks.model;
+
+/**
+ * Created by steve on 12/28/16.
+ */
+
+public class ProductionCountries {
+    private String iso_3166_1;
+    private String name;
+
+    public String getIso_3166_1() {
+        return iso_3166_1;
+    }
+
+    public void setIso_3166_1(String iso_3166_1) {
+        this.iso_3166_1 = iso_3166_1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
